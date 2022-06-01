5b30865ad5d54ad9fca4dd52ea52f9cbc4cd426b
Danilo Sousa <danilo.souza@sergipetec.org.br>
2022-06-01 10:49:27 -0300

    Inicializando Projeto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
 export const environment = {
-  production: true
+  production: false
 };
+
+export const SpotifyConfifuration = {
+  clientId: "15118f7ac6554e90b7eec1c1182333d6",
+  authEndPoint: "https://accounts.spotify.com/authorize",
+  redirectUrl: "http://localhost:4200/login",
+  scopes: [
+    "user-read-currently-playing", // ler música tocando agora
+    "user-read-recently-played", // ler músicas tocadas recentemente
+    "user-read-playback-state", // ler estado do player do usuário
+    "user-top-read", // top artistas e musicas do usuario
+    "user-modify-playback-state", // alteração do player do usuário
+    "user-library-read", // ler biblioteca do usuario
+    "playlist-read-private", // ler playlist privada
+    "playlist-read-collaborative" // ler playlist colaborativa
+  ]
+}
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,20 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+export const SpotifyConfifuration = {
+  clientId: "15118f7ac6554e90b7eec1c1182333d6",
+  authEndPoint: "https://accounts.spotify.com/authorize",
+  redirectUrl: "http://localhost:4200/login",
+  scopes: [
+    "user-read-currently-playing", // ler música tocando agora
+    "user-read-recently-played", // ler músicas tocadas recentemente
+    "user-read-playback-state", // ler estado do player do usuário
+    "user-top-read", // top artistas e musicas do usuario
+    "user-modify-playback-state", // alteração do player do usuário
+    "user-library-read", // ler biblioteca do usuario
+    "playlist-read-private", // ler playlist privada
+    "playlist-read-collaborative" // ler playlist colaborativa
+  ]
+}
+
