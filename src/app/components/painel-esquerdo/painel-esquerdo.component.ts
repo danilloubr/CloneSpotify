08296f9fb7fde66ac202a6a0c0005029a6d59134
Danilo Sousa <danilo.souza@sergipetec.org.br>
2022-06-04 21:45:20 -0300

    Consumindo api para trazer playlist do usuário

--- src/app/components/painel-esquerdo/painel-esquerdo.component.ts
+++ src/app/components/painel-esquerdo/painel-esquerdo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faGuitar, faHome, faMusic, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { IPlaylist } from 'src/app/interfaces/IPlaylist';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-painel-esquerdo',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelEsquerdoComponent implements OnInit {
 
-  constructor() { }
+  iconeHome = faHome
+  iconePesquisar = faSearch
+  iconeArtirtas = faGuitar
+  iconePlaylist = faMusic
+
+  menuSelecionado = 'Home'
+
+  playList: IPlaylist[] = []
+
+  constructor( private spotifyService: SpotifyService) { }
 
   ngOnInit(): void {
+    this.buscarPlayList()
+  }
+
+  async buscarPlayList(){
+    this.playList =  await this.spotifyService.buscarPlayListUsuario()
+    console.log("lista:", this.playList)
+  }
+
+  botaoClique(event: string){
+this.menuSelecionado = event
   }
 
 }
--- src/app/pages/player/player.module.ts
+++ src/app/pages/player/player.module.ts
@@ -4,17 +4,21 @@ import { PlayerComponent } from './player.component';
 import { RouterModule } from '@angular/router';
 import { PlayerRotas } from './player.routers';
 import { PainelEsquerdoComponent } from 'src/app/components/painel-esquerdo/painel-esquerdo.component';
+import { BotaoMenuComponent } from 'src/app/components/botao-menu/botao-menu.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 
 @NgModule({
   declarations: [
     PlayerComponent,
-    PainelEsquerdoComponent
+    PainelEsquerdoComponent,
+    BotaoMenuComponent
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(PlayerRotas)
+    RouterModule.forChild(PlayerRotas),
+    FontAwesomeModule
   ]
 })
 export class PlayerModule { }
