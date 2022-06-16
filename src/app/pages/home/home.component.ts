5279c617318f131e3c7b40bfc6fff515dfc7c666
Danilo Sousa <danilo.souza@sergipetec.org.br>
2022-06-16 00:03:58 -0300

    Finalizando musica tocando atual

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import { Subscription } from 'rxjs';
+import { newMusica } from 'src/app/common/factory';
 import { IMusicaCurtida } from 'src/app/interfaces/IMusicaCurtida';
+import { PlayerService } from 'src/app/services/player.service';
 import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
@@ -8,20 +11,39 @@ import { SpotifyService } from 'src/app/services/spotify.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   musicas: IMusicaCurtida[] = []
+  musicaAtual: IMusicaCurtida = newMusica()
   iconPlayer = faPlay
 
-  constructor(private spotifyService: SpotifyService) { }
+  subs: Subscription[] = []
 
+  constructor(private spotifyService: SpotifyService, private playerService: PlayerService) { }
+  
   ngOnInit(): void {
     this.buscarMusicasCurtidas()
+    this.obterMusicaAtual()
+  }
+  
+  ngOnDestroy(): void {
+    this.subs.forEach(subs => {
+      subs.unsubscribe()
+    })
   }
 
   async buscarMusicasCurtidas(){
     this.musicas =  await this.spotifyService.buscarMusicasCurtidas()
     console.log("MUSICAS", this.musicas)
+  }
+
+  obterMusicaAtual(){
+    const sub = this.playerService.musicaAtual.subscribe(musica => {
+      this.musicaAtual = musica
+      console.log("musica atual:", musica)
+    })
+
+    this.subs.push(sub)
   }
 
   obterArtista(musica: IMusicaCurtida){
@@ -30,6 +52,7 @@ export class HomeComponent implements OnInit {
 
   async executarMusica(musica: IMusicaCurtida) {
     await this.spotifyService.executarMusica(musica.id)
+    this.playerService.definirMusicaAtual(musica)
   }
 
 }
